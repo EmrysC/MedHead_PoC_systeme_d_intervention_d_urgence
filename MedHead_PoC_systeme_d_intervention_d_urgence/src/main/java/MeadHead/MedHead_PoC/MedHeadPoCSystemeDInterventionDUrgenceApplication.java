5caccac3e858b3cfd591c2d840c28a04e8c6791d
EmrysC <emrys38@gmail.com>
2025-12-02 18:20:10 +0100

    init spring

---
+++ MedHead_PoC_systeme_d_intervention_d_urgence/src/main/java/MeadHead/MedHead_PoC/MedHeadPoCSystemeDInterventionDUrgenceApplication.java
@@ -0,0 +1,13 @@
+package MeadHead.MedHead_PoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHeadPoCSystemeDInterventionDUrgenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedHeadPoCSystemeDInterventionDUrgenceApplication.class, args);
+	}
+
+}
---
+++ MedHead_PoC_systeme_d_intervention_d_urgence/src/test/java/MeadHead/MedHead_PoC/MedHeadPoCSystemeDInterventionDUrgenceApplicationTests.java
@@ -0,0 +1,13 @@
+package MeadHead.MedHead_PoC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedHeadPoCSystemeDInterventionDUrgenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
