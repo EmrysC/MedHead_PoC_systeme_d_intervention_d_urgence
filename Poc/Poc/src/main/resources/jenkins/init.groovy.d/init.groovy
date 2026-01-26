f2c40a10c092f4ef01cca2dd60c8ae552d996cb8
EmrysC <emrys38@gmail.com>
2026-01-26 16:22:46 +0100

    jenkins pipeline

--- Poc/Poc/src/main/resources/jenkins/init.groovy.d/init.groovy
+++ Poc/Poc/src/main/resources/jenkins/init.groovy.d/init.groovy
@@ -4,36 +4,39 @@ import java.io.File
 import java.io.ByteArrayInputStream
 import javax.xml.transform.stream.StreamSource
 
-// On execute dans un thread pour ne pas bloquer le demarrage de l'interface UI
 Thread.start {
-    // pause de 15s pour que Jenkins charge ses modules de base
-    println "--- [INIT] Attente de 15s pour stabilisation ---"
+    println "--- [INIT] Attente de 15s ---"
     sleep(15000) 
     
     def jobName = "MedHead_Pipeline"
     def instance = Jenkins.getInstance()
-    // Chemin interne au conteneur (mappé via ton volume docker-compose)
     def xmlPath = "/var/jenkins_home/init.groovy.d/job_config.xml"
     def xmlFile = new File(xmlPath)
-
-    println "--- [INIT] Verification de la configuration du Job ---"
 
     try {
         if (xmlFile.exists()) {
             def jobXml = xmlFile.text
-            
-            // On verifie si le job existe déjà
             def existingJob = instance.getItem(jobName)
             
             if (existingJob != null) {
-                println "--- [INFO] Le job existe deja : Mise a jour de la configuration ---"
                 def xmlStream = new ByteArrayInputStream(jobXml.getBytes("UTF-8"))
                 existingJob.updateByXml(new StreamSource(xmlStream))
                 existingJob.save()
             } else {
-                println "--- [INFO] Creation du nouveau job : ${jobName} ---"
                 def xmlStream = new ByteArrayInputStream(jobXml.getBytes("UTF-8"))
                 instance.createProjectFromXML(jobName, xmlStream)
             }
 
-            // SAUVEGARDE GLOBALE : Empeche le crash au redemarrage
+            instance.save()
+            sleep(2000)
+            
+            def job = instance.getItem(jobName)
+            if (job != null) {
+                job.scheduleBuild2(0)
+                println "--- [SUCCES] Build lance ---"
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace()
+    }
+}
