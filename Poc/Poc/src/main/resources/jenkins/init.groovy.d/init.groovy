3de81c05ad709c72d37c1fe0660b730f1a3fa3f3
EmrysC <emrys38@gmail.com>
2026-01-27 09:40:05 +0100

    SonarQube configuration

--- Poc/Poc/src/main/resources/jenkins/init.groovy.d/init.groovy
+++ Poc/Poc/src/main/resources/jenkins/init.groovy.d/init.groovy
@@ -5,8 +5,9 @@ import java.io.ByteArrayInputStream
 import javax.xml.transform.stream.StreamSource
 
 Thread.start {
-    println "--- [INIT] Attente de 15s ---"
-    sleep(15000) 
+    // Augmenté à 30s pour laisser le temps aux plugins Docker/Git de charger
+    println "--- [INIT] Attente de 30s pour stabilisation Jenkins ---"
+    sleep(30000) 
     
     def jobName = "MedHead_Pipeline"
     def instance = Jenkins.getInstance()
@@ -15,28 +16,43 @@ Thread.start {
 
     try {
         if (xmlFile.exists()) {
+            println "--- [INIT] Fichier config trouvé, configuration du Job: ${jobName} ---"
             def jobXml = xmlFile.text
             def existingJob = instance.getItem(jobName)
             
             if (existingJob != null) {
+                println "--- [INIT] Mise à jour du job existant ---"
                 def xmlStream = new ByteArrayInputStream(jobXml.getBytes("UTF-8"))
                 existingJob.updateByXml(new StreamSource(xmlStream))
                 existingJob.save()
             } else {
+                println "--- [INIT] Création du nouveau job ---"
                 def xmlStream = new ByteArrayInputStream(jobXml.getBytes("UTF-8"))
                 instance.createProjectFromXML(jobName, xmlStream)
             }
 
+            // Forcer Jenkins à sauvegarder l'état global
             instance.save()
-            sleep(2000)
+            // Recharger la configuration pour être sûr que le XML est bien pris en compte
+            instance.reload() 
+            
+            sleep(5000) // Petit délai après reload
             
             def job = instance.getItem(jobName)
             if (job != null) {
-                job.scheduleBuild2(0)
-                println "--- [SUCCES] Build lance ---"
+                // On vérifie si un build n'est pas déjà en cours avant d'en lancer un
+                if (!job.isBuilding() && !job.isInQueue()) {
+                    job.scheduleBuild2(0)
+                    println "--- [SUCCES] Job ${jobName} configuré et build lancé ---"
+                } else {
+                    println "--- [INFO] Job déjà en cours d'exécution, pas de nouveau build lancé ---"
+                }
             }
+        } else {
+            println "--- [ERREUR] Fichier ${xmlPath} introuvable ---"
         }
     } catch (Exception e) {
+        println "--- [ERREUR] Échec de l'init du job : ${e.message} ---"
         e.printStackTrace()
     }
 }
