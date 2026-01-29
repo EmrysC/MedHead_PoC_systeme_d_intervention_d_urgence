98beaac5e63e80a06365ec887ffbab32f58ff6eb
EmrysC <emrys38@gmail.com>
2026-01-29 11:06:49 +0100

    pipeline sonarcube 18

--- Poc/Poc/src/main/resources/jenkins/init.groovy.d/init.groovy
+++ Poc/Poc/src/main/resources/jenkins/init.groovy.d/init.groovy
@@ -4,84 +4,55 @@ import hudson.plugins.sonar.*
 import hudson.plugins.sonar.model.*
 import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.domains.*
-import com.cloudbees.plugins.credentials.impl.*
 import hudson.util.Secret
 import java.io.ByteArrayInputStream
 import javax.xml.transform.stream.StreamSource
 
-// On lance l'initialisation dans un thread séparé pour ne pas bloquer le démarrage de Jenkins
+// Initialisation asynchrone pour ne pas bloquer le démarrage de Jenkins
 Thread.start {
-    println "--- [INIT] Attente de 30s pour stabilisation de Jenkins et des plugins ---"
+    println "--- [INIT] Attente de 30s pour stabilisation de Jenkins ---"
     sleep(30000) 
 
     def instance = Jenkins.get()
     
     // ========================================================================
-    // PARTIE 1 : CONFIGURATION DES IDENTIFIANTS (Credentials)
-    // ========================================================================
-    def sonarCredsId = "sonarqube-admin" 
-    try {
-        println "--- [INIT] Configuration des credentials 'sonarqube-admin' ---"
-        
-        def credentialsStore = instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
-        def domain = Domain.global()
-        
-        def sonarCredential = new UsernamePasswordCredentialsImpl(
-            CredentialsScope.GLOBAL,
-            sonarCredsId,
-            "Authentification automatique admin/admin pour SonarQube",
-            "admin",
-            "admin"
-        )
-
-        def existingCreds = credentialsStore.getCredentials(domain).find { it.id == sonarCredsId }
-        if (existingCreds) {
-            credentialsStore.updateCredentials(domain, existingCreds, sonarCredential)
-            println "--- [INIT] Credentials existants mis à jour ---"
-        } else {
-            credentialsStore.addCredentials(domain, sonarCredential)
-            println "--- [INIT] Nouveaux credentials créés ---"
-        }
-
-    } catch (Exception e) {
-        println "--- [ERREUR] Partie Credentials : ${e.message} ---"
-    }
-
-    // ========================================================================
-    // PARTIE 2 : ENREGISTREMENT DU SERVEUR SONARQUBE
+    // PARTIE 1 : ENREGISTREMENT DU SERVEUR SONARQUBE
     // ========================================================================
     try {
-        println "--- [INIT] Enregistrement du serveur SonarQube 'sonarqube-poc' ---"
+        println "--- [INIT] Liaison du serveur 'sonarqube-poc' au token 'SONARCUBE_TOKEN' ---"
         
         def sonarServerName = "sonarqube-poc"
         def sonarServerUrl = "http://sonarqube-poc:9000"
+        def sonarCredsId = "SONARCUBE_TOKEN" // Utilisation de votre ID existant
         
         def sonarGlobalConfig = instance.getDescriptorByType(SonarGlobalConfiguration.class)
         
+        // On crée l'installation en pointant sur votre credential "Secret Text"
         def sonarInst = new SonarInstallation(
-            sonarServerName,    // Nom utilisé dans le Jenkinsfile
-            sonarServerUrl,     // URL interne Docker
-            sonarCredsId,       // Utilisation de l'ID "sonarqube-admin"
-            null,               // Token (vide car login/pass utilisés)
-            null,               // Webhook Secret
+            sonarServerName,    // Name
+            sonarServerUrl,     // Server URL
+            sonarCredsId,       // Credentials ID (votre token Jenkins)
+            null,               // Webhook Secret ID
             null,               // Extra Properties
             null,               // Triggers
             null,               // Mojo Version
-            null                // Additional Analysis Properties
+            null,               // Additional Analysis Properties
+            null                // Additional Analysis Properties (versioning plugin)
         )
         
+        // Application de la configuration (en tableau pour la compatibilité)
         sonarGlobalConfig.setInstallations([sonarInst] as SonarInstallation[])
         sonarGlobalConfig.save()
         instance.save()
         
-        println "--- [INIT] Serveur SonarQube '${sonarServerName}' activé avec succès ---"
+        println "--- [INIT] Serveur '${sonarServerName}' configuré avec succès ---"
 
     } catch (Exception e) {
         println "--- [ERREUR] Configuration Serveur Sonar : ${e.message} ---"
     }
 
     // ========================================================================
-    // PARTIE 3 : CONFIGURATION DU JOB (Pipeline)
+    // PARTIE 2 : CONFIGURATION DU JOB (Pipeline)
     // ========================================================================
     def jobName = "MedHead_Pipeline"
     def xmlPath = "/var/jenkins_home/init.groovy.d/job_config.xml"
@@ -89,7 +60,7 @@ Thread.start {
 
     try {
         if (xmlFile.exists()) {
-            println "--- [INIT] Configuration du Job : ${jobName} ---"
+            println "--- [INIT] Synchronisation du Job : ${jobName} ---"
             def jobXml = xmlFile.text
             def existingJob = instance.getItem(jobName)
             
@@ -97,19 +68,23 @@ Thread.start {
                 def xmlStream = new ByteArrayInputStream(jobXml.getBytes("UTF-8"))
                 existingJob.updateByXml(new StreamSource(xmlStream))
                 existingJob.save()
+                println "--- [INIT] Job mis à jour ---"
             } else {
                 def xmlStream = new ByteArrayInputStream(jobXml.getBytes("UTF-8"))
                 instance.createProjectFromXML(jobName, xmlStream)
+                println "--- [INIT] Job créé ---"
             }
 
             instance.save()
-            sleep(5000) 
+            sleep(5000) // Petit délai avant le premier build
             
             def job = instance.getItem(jobName)
             if (job != null && !job.isBuilding() && !job.isInQueue()) {
                 job.scheduleBuild2(0)
                 println "--- [SUCCÈS] Job '${jobName}' prêt et build lancé ---"
             }
+        } else {
+            println "--- [ALERTE] Fichier ${xmlPath} introuvable, job non configuré ---"
         }
     } catch (Exception e) {
         println "--- [ERREUR] Initialisation du Job : ${e.message} ---"
